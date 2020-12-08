233830ee8d9ee9e72218a3de601a92389ec01231
mikael-d <etudiant@teleporteur.oclock>
2020-12-08 21:24:10 +0100

    gatsby image

--- src/examples/Images.js
+++ src/examples/Images.js
@@ -1,11 +1,47 @@
 import React from 'react'
 import Layout from '../components/layout'
+import {graphql,useStaticQuery} from "gatsby"
+import img from '../images/dols.webp'
+import Image from 'gatsby-image'
+
+const getImages = graphql`
+  {
+    file(relativePath: { eq: "dols.webp" }) {
+      childImageSharp {
+        fixed(width: 200, grayscale: true) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+    fluid: file(relativePath: { eq: "wave.webp" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
+
 
 const Images = () => {
+    const data = useStaticQuery(getImages);
+    console.log('data',data);
     return (
-        <div>
-            Hello from the image example
-        </div>
+      <section className="images">
+        <article className="single-image">
+          <h3>basic image</h3>
+          <img src={img} width="100%" alt="basic-image"/>
+        </article>
+        <article className="single-image">
+          <h3>fixed image/blur</h3>
+          <Image fixed={data.file.childImageSharp.fixed}/>
+        </article>
+        <article className="single-image">
+          <h3>fluid image/svg</h3>
+          <Image fluid={data.fluid.childImageSharp.fluid}/>
+        </article>
+      </section>
     )
 }
 
